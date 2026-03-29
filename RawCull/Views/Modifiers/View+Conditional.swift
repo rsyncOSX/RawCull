220b81ef0842b006d270526ebd0d2f7052a79fe2
Thomas Evensen <thomeven@gmail.com>
2026-03-29 18:02:52 +0200

    Remove conditional view and obsolete modifiers
    Delete View+Conditional.swift (removing the View.if conditional builder) and remove several unused modifiers/types from Viewmodifiers.swift: FixedTag, DismissafterMessageView, and MessageView. This cleans up dead/duplicated UI utility code while leaving ToggleViewDefault intact.

--- RawCull/Views/Modifiers/View+Conditional.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-extension View {
-    @ViewBuilder
-    func `if`(_ condition: Bool, transform: (Self) -> some View) -> some View {
-        if condition { transform(self) } else { self }
-    }
-}
