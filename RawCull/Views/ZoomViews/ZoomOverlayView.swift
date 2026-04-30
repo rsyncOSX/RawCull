fed2246bcee5376044227faa000404e78bafb6fc
Thomas Evensen <thomeven@gmail.com>
2026-04-30 13:49:17 +0200

    Toogle JPG extract

--- RawCull/Views/ZoomViews/ZoomOverlayView.swift
+++ RawCull/Views/ZoomViews/ZoomOverlayView.swift
@@ -99,10 +99,11 @@ struct ZoomOverlayView: View {
         .focusable()
         .focused($isImageFocused)
         .focusEffectDisabled(true)
-        .onKeyPress(characters: CharacterSet(charactersIn: "+-")) { press in
+        .onKeyPress(characters: CharacterSet(charactersIn: "+-jJ")) { press in
             switch press.characters {
             case "+": increaseZoom(); return .handled
             case "-": decreaseZoom(); return .handled
+            case "j", "J": useThumbnailSource.toggle(); return .handled
             default: return .ignored
             }
         }
