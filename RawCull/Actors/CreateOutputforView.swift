2a809806c15945556f212ce409f4e28d59132678
Thomas Evensen <thomeven@gmail.com>
2026-05-29 18:39:14 +0200

    update

--- RawCull/Actors/CreateOutputforView.swift
+++ RawCull/Actors/CreateOutputforView.swift
@@ -1,15 +1,16 @@
 //
-//  ActorCreateOutputforView.swift
+//  CreateOutputforView.swift
 //  RawCull
 //
 //  Created by Thomas Evensen on 31/01/2026.
 //
 import OSLog
 
-struct CreateOutputforView {
+struct CreateOutputforView: Sendable {
     /// From Array[String]
+    @concurrent
     func createOutputForView(_ stringoutputfromrsync: [String]?) async -> [RsyncOutputData] {
-        Logger.process.debugThreadOnly("ActorCreateOutputforView: createaoutputforview()")
+        Logger.process.debugThreadOnly("CreateOutputforView: createaoutputforview()")
         if let stringoutputfromrsync {
             return stringoutputfromrsync.map { line in
                 RsyncOutputData(record: line)
