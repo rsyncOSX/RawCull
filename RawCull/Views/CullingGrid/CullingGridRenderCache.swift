5c5301af4b0d6991bba7859e3c6df8072c0ce724
Thomas Evensen <thomeven@gmail.com>
2026-07-23 07:26:24 +0200

    Burst Group sorted by rank

--- RawCull/Views/CullingGrid/CullingGridRenderCache.swift
+++ RawCull/Views/CullingGrid/CullingGridRenderCache.swift
@@ -93,19 +93,19 @@ struct CullingGridRenderCache {
 }
 
 enum BurstGroupCleanViewPolicy {
-    static let visibleLimit = 4
+    static let visibleLimit = 3
 
     static func visibleFiles(
         in files: [FileItem],
         rankedFileIDs: [FileItem.ID],
         isCollapsed: Bool,
     ) -> [FileItem] {
-        guard isCollapsed, files.count > visibleLimit else { return files }
-
         let filesByID = Dictionary(uniqueKeysWithValues: files.map { ($0.id, $0) })
         var ordered = rankedFileIDs.compactMap { filesByID[$0] }
         let rankedIDs = Set(ordered.map(\.id))
         ordered.append(contentsOf: files.filter { !rankedIDs.contains($0.id) })
+
+        guard isCollapsed, ordered.count > visibleLimit else { return ordered }
         return Array(ordered.prefix(visibleLimit))
     }
 }
