a3da9c0456214ebfbfde435b547125fdcb70c160
Thomas Evensen <thomeven@gmail.com>
2026-09-22 16:51:48 +0200

    models

--- RawCull/Views/Histogram/HistogramView.swift
+++ RawCull/Views/Histogram/HistogramView.swift
@@ -67,44 +67,6 @@ nonisolated enum HistogramLoader {
     }
 }
 
-@MainActor
-@Observable
-final class HistogramPresentationModel {
-    private(set) var normalizedBins: [CGFloat] = []
-
-    func load(
-        image: NSImage?,
-        calculate: HistogramLoader.Calculator = HistogramLoader.calculate,
-    ) async {
-        guard let image else {
-            normalizedBins = []
-            return
-        }
-        guard let cgImage = image.cgImage(
-            forProposedRect: nil,
-            context: nil,
-            hints: nil,
-        ) else {
-            normalizedBins = []
-            Logger.process.warning("Could not initialize CGImage from NSImage")
-            return
-        }
-
-        do {
-            let bins = try await calculate(cgImage)
-            try Task.checkCancellation()
-            normalizedBins = bins
-        } catch is CancellationError {
-            // A newer image owns publication after SwiftUI cancels this task.
-        } catch {
-            guard !Task.isCancelled else { return }
-            normalizedBins = []
-            Logger.process.warning(
-                "Could not calculate image histogram: \(String(describing: error), privacy: .public)",
-            )
-        }
-    }
-}
 
 struct HistogramView: View {
     let nsImage: NSImage?
